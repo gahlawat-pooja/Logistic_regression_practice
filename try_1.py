dcb6b4c0d333ff1892822456a4ce0481ff994ac0
gahlawat08 <gahlawatpooja2002@gmail.com>
2024-12-22 20:49:19 +0530

    integer is upto 500 and image with labels

--- try_1.py
+++ try_1.py
@@ -5,8 +5,13 @@ def sqrt(x):
     return np.sqrt(x)
 # main function
 def main():
-    x = np.linspace(0, 100, 100)
+    x = np.linspace(0, 500, 200)
     y = sqrt(x)
+    # add x and y labels
+    plt.xlabel('Integers')
+    plt.ylabel('Square root')
+    # fig title
+    plt.title('Square root of integers')
     plt.plot(x, y)
     plt.show()
 if __name__ == '__main__':
